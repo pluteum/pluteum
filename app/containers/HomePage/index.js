f36196a57a0d30d91edefd7e22dc06f3b18cd94a
George Sumpster <gsumpster@gmail.com>
2020-04-25 22:57:27 +0000

    removed book index

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,7 +9,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Breadcrumb from 'components/layout/breadcrumbs/Breadcrumbs';
-import BookIndex from '../BookIndex';
 
 const Layout = styled.div``;
 
@@ -17,7 +16,6 @@ export default function HomePage() {
   return (
     <Layout>
       <Breadcrumb />
-      <BookIndex />
     </Layout>
   );
 }
