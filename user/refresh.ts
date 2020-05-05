294829c724bdb1a891ec7b6854b1c480eba5f0dd
George Sumpster <gsumpster@gmail.com>
2020-05-05 02:24:58 +0000

    added try/catch on refresh

--- user/refresh.ts
+++ user/refresh.ts
@@ -12,33 +12,34 @@ const JWT_KEY = process.env.JWT_KEY || "default";
 export default async function refreshHandler(req: Request, res: Response) {
   // Validate request
   const pool = getDb();
-  const token = jwt.verify(req.cookies["accesscard-refresh"], JWT_KEY);
+  try {
+    const token = jwt.verify(req.cookies["accesscard-refresh"], JWT_KEY);
 
-  if (typeof token === "object") {
-    const { id, library, jti } = token as any;
+    if (typeof token === "object") {
+      const { id, library, jti } = token as any;
 
-    const query = select(
-      "id",
-      "uuid",
-      "firstName",
-      "lastName",
-      "email",
-      "createdAt"
-    )
-      .from("users")
-      .where({ id, refreshToken: jti })
-      .toParams();
+      const query = select(
+        "id",
+        "uuid",
+        "firstName",
+        "lastName",
+        "email",
+        "createdAt"
+      )
+        .from("users")
+        .where({ id, refreshToken: jti })
+        .toParams();
 
-    const user = await pool.query(query).then((result) => result.rows[0]);
-    const refresh = await generateRefreshToken(user, library);
+      const user = await pool.query(query).then((result) => result.rows[0]);
+      const refresh = await generateRefreshToken(user, library);
 
-    res
-      .status(200)
-      .cookie("accesscard-refresh", refresh)
-      .send({ token: await generateToken(user, library), user });
+      res
+        .status(200)
+        .cookie("accesscard-refresh", refresh, { httpOnly: true })
+        .send({ token: await generateToken(user, library) });
+    }
+  } catch (e) {
+    refreshDebug(e.message);
+    res.status(401).send();
   }
-
-  //   const { refresh, ...auth } = await loginUser(body);
-
-  //   res.status(200).cookie("accesscard-refresh", refresh).send(auth);
 }
