546fa69abfd69ae18d9b343a44070a1a5382b4e9
George Sumpster <gsumpster@gmail.com>
2020-05-18 03:48:09 +0000

    removed old tests

--- cover/app/containers/Settings/LibraryManagement/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectSettingsLibraryManagementDomain } from '../selectors';
-
-describe('selectSettingsLibraryManagementDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
