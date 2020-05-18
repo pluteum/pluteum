801177879079f50460186fb273f8e3501ce4e0d1
George Sumpster <gsumpster@gmail.com>
2020-05-18 05:16:12 +0000

    seperated business logic out from ui for login

--- cover/app/containers/Login/index.js
+++ cover/app/containers/Login/index.js
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Logo from 'components/common/Logo/Logo';
-import Typography from 'components/common/Type/Typography';
-import Button from 'components/form/Button';
-import Checkbox from 'components/form/input/Checkbox';
-import TextInput from 'components/form/input/Text';
+
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 import { LoginSchema } from './validation';
-import { Layout, Box, StyledError } from './styles';
 import { loginMutation } from './queries';
+import LoginUI from './ui';
 
 const ERRORS = {
   UNAUTHORIZED: 'The username or password you entered is incorrect',
@@ -68,44 +61,7 @@ export default function Login({ setJWT, history }) {
     }
   }
 
-  return (
-    <Layout>
-      <Box>
-        <Link to="/login">
-          <Logo />
-        </Link>
-        <form onSubmit={onSubmit}>
-          <Typography type="SectionTitle">Sign In</Typography>
-          <TextInput
-            disabled={loading}
-            name="email"
-            label="Email Address"
-            error={errors.email}
-          />
-          <TextInput
-            disabled={loading}
-            name="password"
-            label="Password"
-            type="password"
-            error={errors.password}
-          />
-          <Checkbox disabled={loading} name="rememberme" label="Remember Me" />
-          <Button>
-            {loading ? (
-              <FontAwesomeIcon className="spinner" icon={faSpinner} />
-            ) : (
-              'Login'
-            )}
-          </Button>
-          <Typography style={{ marginLeft: 15 }} type="TextLink" to="/forgot">
-            Forgot your password?
-          </Typography>
-          {errors.form && <StyledError>{errors.form}</StyledError>}
-        </form>
-        <p>Developed by George Sumpster / Designed by Johnny Lee</p>
-      </Box>
-    </Layout>
-  );
+  return <LoginUI onSubmit={onSubmit} loading={loading} errors={errors} />;
 }
 
 Login.propTypes = {
---
+++ cover/app/containers/Login/ui.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Typography from 'components/common/Type/Typography';
+import TextInput from 'components/form/input/Text';
+import PropTypes from 'prop-types';
+import Logo from 'components/common/Logo/Logo';
+import Checkbox from 'components/form/input/Checkbox';
+import Button from 'components/form/Button';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import { Layout, Box, StyledError } from './styles';
+
+export default function LoginUI({ onSubmit, loading, errors }) {
+  return (
+    <Layout>
+      <Box>
+        <Link to="/login">
+          <Logo />
+        </Link>
+        <form onSubmit={onSubmit}>
+          <Typography type="SectionTitle">Sign In</Typography>
+          <TextInput
+            disabled={loading}
+            name="email"
+            label="Email Address"
+            error={errors.email}
+          />
+          <TextInput
+            disabled={loading}
+            name="password"
+            label="Password"
+            type="password"
+            error={errors.password}
+          />
+          <Checkbox disabled={loading} name="rememberme" label="Remember Me" />
+          <Button>
+            {loading ? (
+              <FontAwesomeIcon className="spinner" icon={faSpinner} />
+            ) : (
+              'Login'
+            )}
+          </Button>
+          <Typography style={{ marginLeft: 15 }} type="TextLink" to="/forgot">
+            Forgot your password?
+          </Typography>
+          {errors.form && <StyledError>{errors.form}</StyledError>}
+        </form>
+        <p>Developed by George Sumpster / Designed by Johnny Lee</p>
+      </Box>
+    </Layout>
+  );
+}
+
+LoginUI.propTypes = {
+  onSubmit: PropTypes.func,
+  loading: PropTypes.bool,
+  errors: PropTypes.object,
+};
