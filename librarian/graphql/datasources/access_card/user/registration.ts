2027408ba23bf7c57ddb190be7da504466e103de
George Sumpster <gsumpster@gmail.com>
2020-05-23 20:21:28 +0000

    wrote test suite for registration

--- librarian/graphql/datasources/access_card/user/registration.ts
+++ librarian/graphql/datasources/access_card/user/registration.ts
@@ -4,13 +4,51 @@ import { insert } from "sql-bricks";
 import Schema from "validate";
 import { v4 as uuid } from "uuid";
 import debug from "debug";
+import { wrapError, UniqueViolationError } from "db-errors";
 
 import { createLibrary } from "../library/create";
 import { PoolClient } from "pg";
+import { ValidationError, UserInputError } from "apollo-server-express";
 
 const registerDebug = debug("pluteum:accesscard:register");
 
-export default async function registerUser({ firstName, lastName, email, password }: any, pool: PoolClient) {
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const InputSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: EMAIL_REGEX,
+  },
+  password: {
+    type: String,
+    length: { min: 4 },
+    required: true,
+  },
+});
+
+export default async function registerUser(
+  { firstName, lastName, email, password }: any,
+  pool: PoolClient
+) {
+  const validationErrors = InputSchema.validate({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+
+  if (validationErrors.length) {
+    throw new UserInputError("Invalid Registration Input");
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -28,17 +66,26 @@ export default async function registerUser({ firstName, lastName, email, passwor
 
   query.text = `${query.text} RETURNING "id", "firstName", "lastName", "email"`; // returning via postgres
 
-  const user = await pool.query(query).then((result) => result.rows[0]);
+  return pool
+    .query(query)
+    .then((result) => result.rows[0])
+    .then((user) =>
+      createLibrary(
+        {
+          userId: user.id,
+          title: `${user.firstName}'s Library`,
+          defaultLibrary: true,
+        },
+        pool
+      ).then(() => user)
+    )
+    .catch((e) => {
+      if (e.code && e.code === 23505) {
+        throw new ValidationError(
+          `A user with the email address ${email} already exists.`
+        );
+      }
 
-  try {
-    await createLibrary({
-      userId: user.id,
-      title: `${user.firstName}'s Library`,
-      defaultLibrary: true,
-    }, pool);
-  } catch (error) {
-    console.error(error);
-  }
-
-  return user;
+      throw e;
+    });
 }
