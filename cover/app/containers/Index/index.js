fde5fc933b22d773089d4b2b374b31851fb2e7f8
George Sumpster <gsumpster@gmail.com>
2020-06-02 06:43:47 +0000

    wip: implemented query for book index

--- cover/app/containers/Index/index.js
+++ cover/app/containers/Index/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useQuery } from 'react-apollo';
 
 import IndexEmptyState from './EmptyState';
 
+import { GET_BOOKS } from './queries';
+
 export default function Index() {
-  return <IndexEmptyState />;
+  const { data: { books = [] } = {}, loading, error } = useQuery(GET_BOOKS);
+
+  if (books.length > 0) {
+    return (
+      <div>
+        {books.map(book => (
+          <p>
+            {book.title} – {book.author.map(author => author.name).join(', ')}
+          </p>
+        ))}
+      </div>
+    );
+  }
+
+  if (!loading) {
+    return <IndexEmptyState />;
+  }
+
+  return <span />;
 }
