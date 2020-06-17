afe6005512f9c3daadf9d40f871f80a0572d01e9
George Sumpster <gsumpster@gmail.com>
2020-06-17 02:14:22 +0000

    added details call to OpenLibrary fetching

--- services/monocle/fetch/openlibrary.ts
+++ services/monocle/fetch/openlibrary.ts
@@ -3,8 +3,18 @@ import debug from "debug";
 
 const openLibraryDebug = debug("pluteum:monocle:fetch:open_library");
 
-// https://openlibrary.org/api/books?bibkeys=ISBN:1-59327-552-8&format=json&jscmd=data
-export async function getBookByISBN(isbn: any) {
+function makeDataRequest(isbn: any) {
+  const url = "https://openlibrary.org/api/books";
+  const query = {
+    bibkeys: `ISBN:${isbn}`,
+    format: "json",
+    jscmd: "data",
+  };
+
+  return Axios.get(url, { params: query });
+}
+
+function makeDetailsRequest(isbn: any) {
   const url = "https://openlibrary.org/api/books";
   const query = {
     bibkeys: `ISBN:${isbn}`,
@@ -12,14 +22,18 @@ export async function getBookByISBN(isbn: any) {
     jscmd: "details",
   };
 
-  openLibraryDebug(`Requesting book data from ${url} with ISBN ${isbn}`);
-  return Axios.get(url, { params: query })
+  return Axios.get(url, { params: query });
+}
+
+// https://openlibrary.org/api/books?bibkeys=ISBN:1-59327-552-8&format=json&jscmd=data
+export async function getBookByISBN(isbn: any) {
+  openLibraryDebug(`Requesting book data from Open Library with ISBN ${isbn}`);
+  const results: any = { isbn };
+  return makeDataRequest(isbn)
     .then(({ data }) => {
       const isbnKey = Object.keys(data)[0];
 
       if (data[isbnKey]) {
-        const results: any = { isbn };
-
         if (data[isbnKey].title) {
           results.title = data[isbnKey].title;
         }
@@ -29,8 +43,18 @@ export async function getBookByISBN(isbn: any) {
             name: author.name,
           }));
         }
+      } else {
+        throw new Error();
+      }
+    })
+    .then(() => makeDetailsRequest(isbn))
+    .then(({ data }) => {
+      const isbnKey = Object.keys(data)[0];
 
-        return results;
+      if (data[isbnKey]) {
+        if (data[isbnKey].details.description) {
+          results.description = data[isbnKey].details.description;
+        }
       } else {
         throw new Error();
       }
