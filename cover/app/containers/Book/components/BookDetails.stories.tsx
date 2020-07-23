35b452d1c6a4c3cb238fdfb67039ff43e2a8197f
George Sumpster <gsumpster@gmail.com>
2020-07-23 05:46:57 +0000

    added BookDetails presentational component and story

---
+++ cover/app/containers/Book/components/BookDetails.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+import BookDetails from './BookDetails';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Screens/Book Details',
+  component: BookDetails,
+  decorators: [withKnobs],
+};
+
+export const StorybookBookDetails = () => (
+  <BookDetails
+    book={{
+      title: '1Q84',
+      author: 'Haruki Murakami',
+      description:
+        'A young woman named Aomame follows a taxi driver’s enigmatic suggestion and begins to notice puzzling discrepancies in the world around her. She has entered, she realizes, a parallel existence, which she calls 1Q84 —“Q is for ‘question mark.’ A world that bears a question.” Meanwhile, an aspiring writer named Tengo takes on a suspect ghostwriting project. He becomes so wrapped up with the work and its unusual author that, soon, his previously placid life begins to come unraveled.',
+      rating: 4,
+      tags: ['fiction', 'alternate history'],
+    }}
+    onRating={action('On Rating')}
+    onNewTag={action('On New Tag')}
+    onDeleteTag={action('On Delete Tag')}
+  />
+);
+
+StorybookBookDetails.story = {
+  name: 'Book Details',
+};
---
+++ cover/app/containers/Book/components/BookDetails.tsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import BookCover from 'components/BookCover';
+import Rating from 'components/form/Rating';
+import Tags from 'components/form/Tags';
+
+const Layout = styled.div`
+    padding: 10px;
+
+    > div:first-child {
+        float: left;
+        width: 100px;
+
+        margin-right: 15px;
+    }
+
+    @media (min-width: 768px) {
+        display: grid;
+
+        max-width: 1040px;
+        margin: 40px auto;
+
+        grid-template-columns: 1fr 2fr;
+        grid-column-gap: 64px;
+
+        > div:first-child {
+            float: none;
+            width: auto;
+
+            margin-right: unset;
+        }    
+    }
+`;
+
+const TitleLayout = styled.div`
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media (min-width: 768px) {
+        margin-bottom: 35px;
+    }
+`;
+
+const DetailsLayout = styled.div`
+
+`;
+
+const MetaLayout = styled.div`
+    padding-top: 10px;
+    margin-top: 20px;
+
+    border-top: 1px solid ${props => props.theme.colors.lightGrey};
+
+    @media (min-width: 768px) {
+        margin-top: 40px;
+        padding-top: 20px;
+    }
+`;
+
+const FieldGroup = styled.div`
+
+`;
+
+export const BookTitle = styled.h1`
+  font-family: ${props => props.theme.type.display_serif};
+  font-weight: normal;
+  font-size: 48px;
+  line-height: auto;
+  color: ${props => props.theme.colors.black};
+  margin: 0;
+
+  @media (min-width: 768px) {
+    font-size: 76px;
+    line-height: 80px;
+    margin: 15px 0 0;
+  }
+`;
+
+export const Author = styled.p`
+  font-family: ${props => props.theme.type.mono};
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 22px;
+  color: ${props => props.theme.colors.darkGrey};
+  margin: 0;
+  margin-bottom: 0 0 15px;
+
+  @media (min-width: 768px) {
+    font-size: 18px;
+    line-height: 22px;
+    margin: 15px 0 25px;
+  }
+`;
+
+export const Description = styled.p`
+  font-family: ${props => props.theme.type.sans_serif};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: ${props => props.theme.colors.darkGrey};
+  margin: 19px 0 25px;
+
+  @media (min-width: 768px) {
+    font-size: 18px;
+    line-height: 32px
+  }
+`;
+
+export const MetaHeader = styled.h2`
+  font-family: ${props => props.theme.type.sans_serif};
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  color: ${props => props.theme.colors.darkGrey};
+`;
+
+export const FieldHeader = styled.p`
+  font-family: ${props => props.theme.type.sans_serif};
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 22px;
+  color: ${props => props.theme.colors.darkGrey};
+`;
+
+export const FieldContent = styled.p`
+  font-family: ${props => props.theme.type.sans_serif};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  color: ${props => props.theme.colors.darkGrey};
+`;
+
+export default function BookDetails({ book, onRating, onNewTag, onDeleteTag }) {
+  return (
+    <Layout>
+        <div>
+            <BookCover title={book?.title} author={book?.author} />
+        </div>
+        <DetailsLayout>
+            <TitleLayout>
+                <BookTitle>{book?.title}</BookTitle>
+                <Author>by {book?.author}</Author>
+                <Rating rating={book?.rating} onRating={onRating} />
+            </TitleLayout>
+            <Description>{book?.description}</Description>
+            <FieldHeader>Tags</FieldHeader>
+            <Tags tags={book?.tags} onNewTag={onNewTag} onDeleteTag={onDeleteTag} editable={false} />
+            <MetaLayout>
+                <MetaHeader>Information</MetaHeader>
+                <FieldGroup>
+                    <FieldHeader>Series</FieldHeader>
+                    <FieldContent>N / A</FieldContent>
+                </FieldGroup>
+            </MetaLayout>
+        </DetailsLayout>
+    </Layout>
+  );
+}
