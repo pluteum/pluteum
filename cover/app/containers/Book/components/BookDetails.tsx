4305f756609d9f1c2cbcfabf4a18011cfe8e6def
George Sumpster <gsumpster@gmail.com>
2020-08-12 06:20:41 +0000

    cleaning up BookDetails

--- cover/app/containers/Book/components/BookDetails.tsx
+++ cover/app/containers/Book/components/BookDetails.tsx
@@ -5,82 +5,34 @@ import BookCover from 'components/BookCover';
 import PrimaryDetails from './PrimaryDetails';
 import { Formik, Form } from 'formik';
 import Button from 'components/form/Button';
+import BookCoverInput from 'components/form/BookCoverInput';
 
 const Layout = styled.div`
     padding: 10px;
 
+    display: grid;
+
+    max-width: 1040px;
+    margin: 40px auto;
+
+    grid-template-columns: 1fr 2fr;
+    grid-column-gap: 64px;
+
     > div:first-child {
-        float: left;
-        width: 100px;
+        float: none;
+        width: auto;
 
-        margin-right: 15px;
-    }
-
-    @media (min-width: 768px) {
-        display: grid;
-
-        max-width: 1040px;
-        margin: 40px auto;
-
-        grid-template-columns: 1fr 2fr;
-        grid-column-gap: 64px;
-
-        > div:first-child {
-            float: none;
-            width: auto;
-
-            margin-right: unset;
-        }    
-    }
+        margin-right: unset;
+    }    
+    
 `;
 
 const DetailsLayout = styled.div`
     max-width: 640px;
 `;
 
-const MetaLayout = styled.div`
-    padding-top: 10px;
-    margin-top: 20px;
-
-    border-top: 1px solid ${props => props.theme.colors.lightGrey};
-
-    @media (min-width: 768px) {
-        margin-top: 40px;
-        padding-top: 20px;
-    }
-`;
-
-const FieldGroup = styled.div`
-
-`;
-
-export const MetaHeader = styled.h2`
-  font-family: ${props => props.theme.type.sans_serif};
-  font-weight: normal;
-  font-size: 24px;
-  line-height: 28px;
-  color: ${props => props.theme.colors.darkGrey};
-`;
-
-export const FieldHeader = styled.p`
-  font-family: ${props => props.theme.type.sans_serif};
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 22px;
-  color: ${props => props.theme.colors.darkGrey};
-`;
-
-export const FieldContent = styled.p`
-  font-family: ${props => props.theme.type.sans_serif};
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 22px;
-  color: ${props => props.theme.colors.darkGrey};
-`;
-
 export default function BookDetails({ editing, book, onSubmit, onLoadAuthors, onAddAuthor, onLoadTags }) {
   const authorString = !!book.author ? book.author.map((a) => a.name).join(', ') : '';
-  const tags = !!book.tags ? book.tags.map((t) => t.name) : [];
 
   if (editing) {
     return (
@@ -88,7 +40,7 @@ export default function BookDetails({ editing, book, onSubmit, onLoadAuthors, on
         <Form>
           <Layout>
           <div>
-              <BookCover title={book?.title} author={authorString} />
+              {editing ? <BookCoverInput name="image" image={book.image} /> : <BookCover image={book.image} title={book?.title} author={authorString} />}
           </div>
           <DetailsLayout>
               <PrimaryDetails book={book} editing={editing} onLoadAuthors={onLoadAuthors} onAddAuthor={onAddAuthor} onLoadTags={onLoadTags} />
